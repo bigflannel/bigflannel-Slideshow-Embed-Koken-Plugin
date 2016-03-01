26c4b033094de1fa5864d9bf574fa8df8b479460
Bjarne Varöystrand <BlackSkorpio@users.noreply.github.com>
2016-03-01 17:32:33 +0100

    Changed hardcoded lang strings
    Changed hardcoded lang strings to {{ language.string }}
    Also removed the Previous/Next text since we are displaying that in the "tool-tip" for those links

--- bigflannel-slideshow-embed/plugin.php
+++ bigflannel-slideshow-embed/plugin.php
@@ -15,13 +15,13 @@
 	</div>
 	<ul class="nav-content">
 		<li>
-			<a href="#" id="sldshw-prev{$slideUnique}" class="sldshw-prev" title="Previous" data-bind-to-key="left">&larr;&nbsp;Prev</a>
+			<a href="#" id="sldshw-prev{$slideUnique}" class="sldshw-prev" title="{{ language.previous }}" data-bind-to-key="left">&larr;&nbsp;</a>
 		</li>
 		<li>
 			<a href="#" id="sldshw-play{$slideUnique}" class="sldshw-play" title="Toggle" data-bind-to-key="space">Loading</a>
 		</li>
 		<li>
-			<a href="#" id="sldshw-next{$slideUnique}" class="sldshw-next" title="Next" data-bind-to-key="right">Next&nbsp;&rarr;</a>
+			<a href="#" id="sldshw-next{$slideUnique}" class="sldshw-next" title="{{ language.next }}" data-bind-to-key="right">&nbsp;&rarr;</a>
 		</li>
 	</ul>
 	<div class="text-content">
@@ -68,9 +68,9 @@
 		function playState{$slideUnique}(playing) {
 			var el = $('#sldshw-play{$slideUnique}');
 			if (playing) {
-				el.html('Pause');
+				el.html('{{ language.pause }}');
 			} else {
-				el.html('Play');
+				el.html('{{ language.play }}');
 			}
 		}
 	</script>
